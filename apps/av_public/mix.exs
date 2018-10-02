af37c9843f39c603f21fa5d5c0ad8d97bea8ea53
Siddharth Jain <sidjainn@gmail.com>
2018-10-02 15:08:42 +0530

    Create a booking from the search results [ch727]

--- apps/av_public/mix.exs
+++ apps/av_public/mix.exs
@@ -42,7 +42,7 @@ defmodule AVPublic.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:kinisi, in_umbrella: true},
+      {:av_data_store, in_umbrella: true},
       {:oauth2, "~> 0.9"}
     ]
   end
